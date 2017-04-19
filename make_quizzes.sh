a85cf25539a014bdb1bc4a53594a71b8cabb527c
gcallah <gcallah@mac.com>
2017-04-19 12:02:15 -0400

    HTML rebuild.

---
+++ make_quizzes.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+quiz2html.py quizzes/quiz7.3.txt >quizzes/quiz7.3.qtml
+quiz2html.py quizzes/quiz7.4.txt >quizzes/quiz7.4.qtml
+quiz2html.py quizzes/quiz7.5.txt >quizzes/quiz7.5.qtml
+quiz2html.py quizzes/quiz7.6.txt >quizzes/quiz7.6.qtml
+quiz2html.py quizzes/quiz7.7.txt >quizzes/quiz7.7.qtml
+quiz2html.py quizzes/quiz7.8.txt >quizzes/quiz7.8.qtml
+quiz2html.py quizzes/quiz7.9.txt >quizzes/quiz7.9.qtml
+quiz2html.py quizzes/quiz7.10.txt >quizzes/quiz7.10.qtml
+quiz2html.py quizzes/quiz7.11.txt >quizzes/quiz7.11.qtml
+quiz2html.py quizzes/quiz7.12.txt >quizzes/quiz7.12.qtml
